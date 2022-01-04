d666c68db2e83c84e7a0968508987ba0189df428
Fordonez20 <44047689+Fordonez20@users.noreply.github.com>
2022-01-04 15:14:01 -0500

    deleted link on homepage

--- Team1-app/src/App.js
+++ Team1-app/src/App.js
@@ -109,14 +109,7 @@ function App() {
         </h3>
         
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn How to Type
-        </a>
+        
       </header>
     </div>
   );
