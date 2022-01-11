912209057b63a17b1a01cac8526151e4cda8e695
Juan Peralta <88753706+JuanP3rAlta23@users.noreply.github.com>
2022-01-11 15:14:47 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -40,36 +40,36 @@ resource "aws_subnet" "web-subnet-3" {
 }
 
 # Create Application Private Subnets
-resource "aws_subnet" "application-subnet-1" {
+resource "aws_subnet" "app-subnet-1" {
   vpc_id                  = aws_vpc.team1-vpc.id
   cidr_block              = "10.0.11.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = false
 
   tags = {
-    Name = "Application-1a"
+    Name = "app-1a"
   }
 }
 
-resource "aws_subnet" "application-subnet-2" {
+resource "aws_subnet" "app-subnet-2" {
   vpc_id                  = aws_vpc.team1-vpc.id
   cidr_block              = "10.0.12.0/24"
   availability_zone       = "us-east-1b"
   map_public_ip_on_launch = false
 
   tags = {
-    Name = "Application-2b"
+    Name = "app-2b"
   }
 }
 
-resource "aws_subnet" "application-subnet-3" {
+resource "aws_subnet" "app-subnet-3" {
   vpc_id                  = aws_vpc.team1-vpc.id
   cidr_block              = "10.0.13.0/24"
   availability_zone       = "us-east-1c"
   map_public_ip_on_launch = false
 
   tags = {
-    Name = "Application-3c"
+    Name = "app-3c"
   }
 }
 
@@ -80,7 +80,7 @@ resource "aws_subnet" "database-subnet-1" {
   availability_zone = "us-east-1a"
 
   tags = {
-    Name = "Database-1a"
+    Name = "DB-1a"
   }
 }
 
@@ -90,7 +90,7 @@ resource "aws_subnet" "database-subnet-2" {
   availability_zone = "us-east-1b"
 
   tags = {
-    Name = "Database-2b"
+    Name = "DB-2b"
   }
 }
 resource "aws_subnet" "database-subnet-3" {
@@ -99,7 +99,7 @@ resource "aws_subnet" "database-subnet-3" {
   availability_zone = "us-east-1c"
 
   tags = {
-    Name = "Database-3c"
+    Name = "DB-3c"
   }
 }
 
@@ -108,10 +108,35 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.team1-vpc.id
 
   tags = {
-    Name = "k1igw"
+    Name = "igw"
   }
 }
 
+ resource "aws_nat_gateway" "natg1" {
+   connectivity_type = "private"
+   subnet_id         = aws_subnet.app-subnet-1.id
+
+     tags = {
+     Name = "App1 NAT"
+   }
+ }
+  resource "aws_nat_gateway" "natg2" {
+   connectivity_type = "private"
+   subnet_id         = aws_subnet.app-subnet-2.id
+
+     tags = {
+     Name = "App2 NAT"
+   }
+ }
+
+ resource "aws_nat_gateway" "natg3" {
+   connectivity_type = "private"
+   subnet_id         = aws_subnet.app-subnet-3.id
+
+     tags = {
+     Name = "App3 NAT"
+   }
+ }
 # Create Web layer route table
 resource "aws_route_table" "web-rt" {
   vpc_id = aws_vpc.team1-vpc.id
@@ -123,22 +148,47 @@ resource "aws_route_table" "web-rt" {
   }
 
   tags = {
-    Name = "k1WebRT"
+    Name = "WebRT"
   }
 }
 
 # Create Web Subnet association with Web route table
-resource "aws_route_table_association" "a" {
+resource "aws_route_table_association" "web-a" {
   subnet_id      = aws_subnet.web-subnet-1.id
   route_table_id = aws_route_table.web-rt.id
 }
 
-resource "aws_route_table_association" "b" {
+resource "aws_route_table_association" "web-b" {
   subnet_id      = aws_subnet.web-subnet-2.id
   route_table_id = aws_route_table.web-rt.id
 }
 
-resource "aws_route_table_association" "c" {
+resource "aws_route_table_association" "web-c" {
   subnet_id      = aws_subnet.web-subnet-3.id
   route_table_id = aws_route_table.web-rt.id
 }
+
+# Creates Application Route Table 
+resource "aws_route_table" "app-rt" {
+   vpc_id = aws_vpc.team1-vpc.id
+   route = []
+   tags = {
+     "Name" = "AppRT"
+   }
+ }
+resource "aws_route_table_association" "app-a" {
+  subnet_id      = aws_subnet.app-subnet-1.id
+  route_table_id = aws_route_table.app-rt.id
+}
+
+resource "aws_route_table_association" "app-b" {
+  subnet_id      = aws_subnet.app-subnet-2.id
+  route_table_id = aws_route_table.app-rt.id
+}
+
+resource "aws_route_table_association" "app-c" {
+  subnet_id      = aws_subnet.app-subnet-3.id
+  route_table_id = aws_route_table.app-rt.id
+}
+
+
