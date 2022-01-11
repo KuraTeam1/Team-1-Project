42485fc5eee7378273049cf5f699456eb64bf90d
Juan Peralta <88753706+JuanP3rAlta23@users.noreply.github.com>
2022-01-11 16:24:00 -0500

    Update securitygroups.tf

--- securitygroups.tf
+++ securitygroups.tf
@@ -76,3 +76,88 @@ resource "aws_security_group" "database-sg" {
   }
 }
 
+resource "aws_security_group" "web-alb-sg" {
+  name        = "web-alb-sg"
+  description = "Security Group for web ALB"
+  vpc_id      = aws_vpc.team1-vpc.id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
+  }
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
+  }
+
+  ingress {
+    from_port = 8080
+    to_port   = 8080
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
+  }
+
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      aws_subnet.web-subnet-1.id,
+      aws_subnet.web-subnet-2.id,
+      aws_subnet.web-subnet-3.id
+    ]
+  }
+
+  egress {
+    from_port = "0"
+    to_port   = "0"
+    protocol  = "-1"
+
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
+  }
+
+  tags = {
+    Name = "public-alb-sg"
+  }
+}
+
+resource "aws_security_group" "appservers-alb-sg" {
+  name        = "appservers-alb-sg"
+  description = "Security Group for app servers ALB"
+  vpc_id      = aws_vpc.team1-vpc.id
+
+  ingress {
+    from_port   = 0             // allow all - don't use for prod
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0             // allow all - don't use for prod
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "appservers-alb-sg"
+  }
+}
