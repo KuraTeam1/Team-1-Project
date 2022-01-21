9bb7eb846b8eb80d3ce3652c60ca7df9736ac6cf
Juan Peralta <88753706+JuanP3rAlta23@users.noreply.github.com>
2022-01-20 21:29:59 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
 provider "aws" {
-  region     = "${var.region}"
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
+  region = "us-east-1"
 }
