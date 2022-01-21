5a692f7d92e932d879db3c64c8bfc3f051f3d3d3
Nabeel Shuaib <49292320+TemporarySolace@users.noreply.github.com>
2022-01-21 11:19:19 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,16 @@ provider "aws" {
   access_key = "AKIAQQU53XAEMF2MR26R"
   secret_key = "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"
 }
+resource "tls_private_key" "pk" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "kp" {
+  key_name   = "myKey"       # Create "myKey" to AWS!!
+  public_key = tls_private_key.pk.public_key_openssh
+
+  provisioner "local-exec" { # Create "myKey.pem" to your computer!!
+    command = "echo '${tls_private_key.pk.private_key_pem}' > ./kurakey.pem"
+  }
+}
