9da12d9126bb753d0082757cec38519cd9444cf2
Juan Peralta <88753706+JuanP3rAlta23@users.noreply.github.com>
2022-01-20 22:01:16 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,8 @@ terraform {
   }
 }
 
-# Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  region     = "${var.region}"
+  access_key = AWS_ACCESS_KEY_ID
+  secret_key = AWS_SECRET_ACCESS_KEY
 }
